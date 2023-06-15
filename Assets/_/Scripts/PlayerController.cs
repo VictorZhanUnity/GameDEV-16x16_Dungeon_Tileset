b9e26bdf36af2dcd363d59d4230c19f6a9c7deb5
VictorZhan <73773024+VictorZhanUnity@users.noreply.github.com>
2023-06-16 01:28:38 +0800

    Update

--- Assets/_/Scripts/PlayerController.cs
+++ Assets/_/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float movement_speed_orignal;
     [Header("Character attributes:")]
-    public float movement_base_speed = 1.0f;
+    public float movement_base_speed = 3.0f;
 
     [Space]
     [Header("Character statistics:")]
@@ -18,11 +19,19 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     public Transform spriteTrans;
 
+    private void Awake()
+    {
+        movement_speed_orignal = movement_base_speed;
+        Debug.Log($"Speed:{animator.speed}");
+    }
+
     private void Update()
     {
         ProcessInput();
         Move();
         Animate();
+
+        SprintInput();
     }
 
     private void ProcessInput()
@@ -30,6 +39,22 @@ public class PlayerController : MonoBehaviour
         movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
         movementDirection.Normalize();
+    }
+
+    private void SprintInput()
+    {
+        float currentSpeed = movement_base_speed;
+
+        if (Input.GetKey(KeyCode.LeftShift) && movementSpeed > 0)
+        {
+            movement_base_speed = movement_speed_orignal * 1.5f;
+            animator.speed = 1.5f;
+        }
+        else
+        {
+            movement_base_speed = movement_speed_orignal;
+            animator.speed = 1;
+        }
     }
 
     private void Move()
@@ -41,7 +66,7 @@ public class PlayerController : MonoBehaviour
     {
         animator.SetFloat("MovementSpeed", movementSpeed);
         float directionX = Input.GetAxis("Horizontal");
-        if(directionX != 0)
+        if (directionX != 0)
         {
             spriteTrans.localScale = new Vector2(directionX > 0 ? 1 : -1, 1);
         }
